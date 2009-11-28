20e3ee80202f7fd3c847e85d1ade731448415d2b
John Constable <john@kript.net>
2009-11-28 22:12:51 +0000

    initial template commit

---
+++ GenerateAdventCalender.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -w
+=head1 NAME
+
+Generate an advent calender page which has links to sub pages which contain images and text.
+
+=head1 VERSION
+
+0.1
+
+=head1 SYNOPSIS
+
+
+=head1 BUGS AND LIMITATIONS
+
+There are no known bugs in this script.
+
+Please report problems to John Constable (John@kript.net)
+
+Patches are welcome. a git repository is forthcoming..
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2009  John Constable (John@kript.net)
+All rights reserved.
+
+This module is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself. See L<perlartistic>.  This program is
+distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.
